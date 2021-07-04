ccd451f24f35d19d68b495353dd461674c8712a8
unknown <benjaminprushin@gmail.com>
2021-07-04 18:17:15 -0400

    Begun database implementaion

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory, session, request, redirect
+from appbase import app, db
+from models.Customer import Customer
 
 @app.route('/')
 def hello_world():
