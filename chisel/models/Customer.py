6b72a4fa4ff22e0486946832dea4eca6584615ff
daseqi <64508533+daseqi@users.noreply.github.com>
2021-07-18 19:25:04 -0400

    customer profile created, bio added

--- chisel/models/Customer.py
+++ chisel/models/Customer.py
@@ -1,13 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from chisel import db, login_manager
+from chisel import app, db, login_manager
 
 class Customer(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    
+    bio = db.Column(db.String(300), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
     def __repr__(self):
         return '<User %r>' % self.username
 
--- chisel/routes.py
+++ chisel/routes.py
@@ -1,12 +1,16 @@
+import os, secrets
 from flask import Flask, render_template, url_for, flash, send_from_directory, session, request, redirect
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from chisel import app, db, bcrypt
 from chisel.models.Customer import Customer
-from chisel.forms import RegistrationForm, LoginForm
+from chisel.forms import RegistrationForm, LoginForm, UpdateProfileForm
+from PIL import Image
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         customer = Customer.query.filter_by(email=form.email.data).first()
@@ -22,10 +26,12 @@ def login():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        customer = Customer(username=form.username.data, email=form.email.data, password=hashed_pw)
+        customer = Customer(username=form.username.data, email=form.email.data, password=hashed_pw, bio="")
         db.session.add(customer)
         db.session.commit()
         flash(f'Welcome to Chisel, {form.username.data}!', 'success')
@@ -44,7 +50,43 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route("/account")
+
+
+
+    
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture = random_hex + f_ext
+    pic_path = os.path.join(app.root_path, 'static/images/profile_pics/', picture)
+
+    output_size = (161, 161) 
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(pic_path)
+
+    return picture
+
+
+@app.route("/profile", methods=['GET', 'POST'])
 @login_required
-def account():
-    return render_template('dashboard.html', title='Account')
+def profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.bio = form.bio.data
+        db.session.commit()
+        flash('Your profile has been updated!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    image_file = url_for('static', filename='images/profile_pics/'+current_user.image_file)
+    return render_template('profile.html', title='Your Profile',
+                           image_file=image_file, form=form)
