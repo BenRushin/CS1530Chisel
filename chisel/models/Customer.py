ea804ff7a491cbf116965c5a59931c4c102b4510
BenRushin <benjaminprushin@gmail.com>
2021-08-05 16:33:35 -0400

    made darkmode have a default value

--- chisel/models/Customer.py
+++ chisel/models/Customer.py
@@ -17,7 +17,7 @@ class Customer(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     posts = db.relationship('Post', backref='author', lazy=True)
     sessions = db.relationship( 'WorkoutSession', backref='customer' )
-    dark_mode = db.Column(db.Boolean(), nullable=True)
+    dark_mode = db.Column(db.Boolean(), nullable=False, default=True)
     modifiers = db.relationship( 'ExerciseModifier', backref='customer' )
 
     followed = db.relationship(
